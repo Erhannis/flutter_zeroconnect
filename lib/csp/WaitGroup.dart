e9d7cd8681a3e9ba8824dda82d383015bbd3ffe8
erhannis <eyeillus@gmail.com>
2022-12-27 12:51:59 -0500

    Ok...that didn't really work.  Trying again.

--- lib/csp/WaitGroup.dart
+++ lib/csp/WaitGroup.dart
@@ -1,44 +1,28 @@
 import "dart:async";
 
-// Modified from `sync` package
-
-/// A WaitGroup waits for a collection of processes to finish.
-/// The main process calls [add] to set the number of processes to wait for.
-/// Then each of the processes runs and calls [done] when finished. At the same
-/// time, [wait] can be used to block until all processes have finished.
 class WaitGroup {
   int _counter = 0;
-  Completer? _completer;
+  Completer<void> _completer = Completer<void>();
 
   WaitGroup();
 
-  /// Adds delta, which may be negative, to the WaitGroup counter.
-  /// If a wait Future is open and the counter becomes zero, the future is
-  /// released.
-  /// Edit: Does not throw if counter goes negative.  Completes whenever <= 0.
-  void add([int amount = 1]) {
-    _counter += amount;
-    final completer = _completer;
-    if (_counter <= 0 && completer != null) {
-      completer.complete();
+  factory WaitGroup.of(int i) => WaitGroup()..add(i);
+
+  void add(int i) {
+    _counter += i;
+  }
+
+  void done() {
+    _counter--;
+    if (_counter == 0) {
+      _completer.complete();
+      _completer = Completer();
     }
   }
 
-  /// Decrements the WaitGroup counter.
-  void done() => add(-1);
-
-  /// Returns a future that will complete when the WaitGroup counter is <= zero.
-  Future wait() {
-    if (_counter <= 0) {
-      return new Future.value();
+  Future<void> wait() async {
+    if (_counter > 0) {
+      return _completer.future;
     }
-
-    final completer = _completer;
-    if (completer == null) {
-      final completer = Completer();
-      _completer = completer;
-      return completer.future;
-    }
-    return completer.future;
   }
 }
